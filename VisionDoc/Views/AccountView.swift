257c316560f856d5c0560d9bd559bb9906726a7a
EugenioPedraza <a00835064@tec.mx>
2024-04-11 12:24:19 -0600

    Tab View

---
+++ VisionDoc/Views/AccountView.swift
@@ -0,0 +1,18 @@
+//
+//  AccountView.swift
+//  VisionDoc
+//
+//  Created by Eugenio Pedraza on 11/04/24.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountView()
+}
