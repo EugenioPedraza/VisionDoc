0f6ba3a2d27f56ecf25b4714d76ce34acef54bd5
Daniel Cantu <93813734+Jdanielcode1@users.noreply.github.com>
2024-04-12 11:39:58 -0600

    Cambios UI

--- VisionDoc/Views/MenuView.swift
+++ VisionDoc/Views/MenuView.swift
@@ -26,7 +26,11 @@ struct MenuView: View {
 struct HomeView: View {
     var body: some View {
         VStack {
-
+            
+            Text("VisionDoc")
+                .font(.custom("SF Pro", size: 130))
+                .fontWeight(.heavy)
+                .padding(.bottom, 500)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .foregroundColor(.white)
@@ -45,11 +49,6 @@ struct HomeView: View {
 struct LearnView: View {
     var body: some View {
         VStack(alignment: .leading) {
-            Text("Body Systems")
-                .font(.custom("monospace", size: 60))
-                .fontWeight(.bold)
-                .padding(.leading, 20)
-                .padding(.top)
             systemsMenu3D(bodySystems: arrBodySystems[0])
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -74,36 +73,9 @@ struct QuizView: View {
     }
 }
 
-struct AccountView: View {
-    @State private var name = ""
-    @State private var classCode = ""
+
     
-    var body: some View {
-        VStack {
-            Form {
-                Section(header: Text("Sign In")) {
-                    TextField("Name", text: $name)
-                    TextField("Class Code", text: $classCode)
-                }
-                
-                Button(action: {
-                    // Perform sign-in action here
-                }) {
-                    Text("Sign In")
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.blue)
-                        .cornerRadius(10)
-                }
-            }
-            .frame(width: 300) // Set a fixed width for the form
-            .padding(.horizontal) // Add horizontal padding
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .foregroundColor(.white)
-        .cornerRadius(20)
-    }
-}
+
 
 #Preview {
     MenuView()
