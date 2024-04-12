bdafadb7b8a9ffbdbc7f041c06f1f3f57b7490e9
Esteban Aleman <esteban21112002@gmail.com>
2024-04-12 12:37:22 -0600

    cambios sprint2

--- VisionDoc/Views/MenuView.swift
+++ VisionDoc/Views/MenuView.swift
@@ -26,7 +26,11 @@ struct MenuView: View {
 struct HomeView: View {
     var body: some View {
         VStack {
-
+            
+            Text("VisionDoc")
+                .font(.custom("SF Pro", size: 130))
+                .fontWeight(.heavy)
+                .padding(.bottom, 500)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .foregroundColor(.white)
@@ -74,36 +78,7 @@ struct QuizView: View {
     }
 }
 
-struct AccountView: View {
-    @State private var name = ""
-    @State private var classCode = ""
-    
-    var body: some View {
-        VStack {
-            Form {
-                Section(header: Text("Sign In")) {
-                    TextField("Name", text: $name)
-                    TextField("Class Code", text: $classCode)
-                }
-                
-                Button(action: {
-                    // Perform sign-in action here
-                }) {
-                    Text("Sign In")
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.blue)
-                        .cornerRadius(10)
-                }
-            }
-            .frame(width: 300) // Set a fixed width for the form
-            .padding(.horizontal) // Add horizontal padding
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .foregroundColor(.white)
-        .cornerRadius(20)
-    }
-}
+
 
 #Preview {
     MenuView()
